51f00e50136faaf4f4fcb885e321d89f4640f1b1
Khaled Silva <khaled.silva17@gmail.com>
2025-12-28 01:18:35 -0300

    feat: adiciona a busca pelos dados do repositório quando usuário aperta o enter

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ const botaoBuscar = document.getElementById('btn-search');
 const buscarUsuario = document.getElementById('input-search');
 const resultadoPerfil = document.querySelector('.profile-results');
 
-botaoBuscar.addEventListener('click', async () => {
+async function obterPerfilUsuario() {
     const nomeUsuario = buscarUsuario.value;
     if (!nomeUsuario) {
         alert('Por favor, digite um nome de usuário do GitHub.');
@@ -15,11 +15,18 @@ botaoBuscar.addEventListener('click', async () => {
     try {
         const dadosUsuario = await buscarUsuarioGithub(nomeUsuario);
         const reposUsuario = await repositoriosUsuario(nomeUsuario);
-        console.log(reposUsuario);
         mostrarPerfil(resultadoPerfil, dadosUsuario, reposUsuario);
     } catch (error) {
         console.error('Erro ao buscar perfil do usuário',error);
         alert('Usuário não encontrado. Por favor, verifique o nome e tente novamente.');
         limparPerfil(resultadoPerfil);
+    }
+}
+
+botaoBuscar.addEventListener('click', obterPerfilUsuario);
+
+buscarUsuario.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        obterPerfilUsuario();
     }
 });
